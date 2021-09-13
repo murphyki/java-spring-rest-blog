2d3ba61cd245cb802f4dd04d80bf699d387be69f
murphyki <kierantmurphy@gmail.com>
2021-09-13 15:51:39 +0100

    Add Author relationships and data - complete

--- src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
+++ src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
@@ -23,22 +23,36 @@ public class DatabaseLoader implements ApplicationRunner {
     public List<Author> authors = new ArrayList<>();
 
     private final PostRepository postRepository;
+    private final AuthorRepository authorRepository;
 
     @Autowired
-    public DatabaseLoader(PostRepository postRepository) {
+    public DatabaseLoader(PostRepository postRepository, AuthorRepository authorRepository) {
         this.postRepository = postRepository;
+        this.authorRepository = authorRepository;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        authors.addAll(Arrays.asList(
+                new Author("sholderness", "Sarah", "Holderness", "password"),
+                new Author("tbell", "Tom", "Bell", "password"),
+                new Author("efisher", "Eric", "Fisher", "password"),
+                new Author("csouza", "Carlos", "Souza", "password")
+        ));
+        this.authorRepository.saveAll(this.authors);
+
         IntStream.range(0,40).forEach(i->{
             String template = templates[i % templates.length];
             String gadget = gadgets[i % gadgets.length];
-
             String title = String.format(template, gadget);
+
             Post post = new Post(title, "Lorem ipsum dolor sit amet, consectetur adipiscing elit… ");
+            Author author = authors.get(i % authors.size());
+            post.setAuthor(author);
+            author.addPost(post);
             randomPosts.add(post);
         });
         this.postRepository.saveAll(this.randomPosts);
+        this.authorRepository.saveAll(this.authors);
     }
 }
--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -3,6 +3,7 @@ package com.pluralsight.blog.data;
 import com.pluralsight.blog.model.Author;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
 
@@ -29,9 +30,11 @@ import java.util.Optional;
 //    }
 //}
 
-@RestResource
+@RepositoryRestResource
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     @RestResource(rel = "contains-title", path = "containsTitle")
     List<Post> findByTitleContaining(String title);
+
+    List<Post> findByAuthor_Lastname(String lastname);
 }
